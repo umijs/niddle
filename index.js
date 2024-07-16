ee8af740f8d8bf8d7170675718e73be7dc34b1fc
xusd320 <xusd320@gmail.com>
2024-07-16 15:29:53 +0800

    fix: ci

--- index.js
+++ index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'niddle.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, '@niddle-rs/niddle.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.android-arm64.node')
+            nativeBinding = require('./@niddle-rs/niddle.android-arm64.node')
           } else {
-            nativeBinding = require('niddle-android-arm64')
+            nativeBinding = require('@niddle-rs/niddle-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'niddle.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, '@niddle-rs/niddle.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.android-arm-eabi.node')
+            nativeBinding = require('./@niddle-rs/niddle.android-arm-eabi.node')
           } else {
-            nativeBinding = require('niddle-android-arm-eabi')
+            nativeBinding = require('@niddle-rs/niddle-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'niddle.win32-x64-msvc.node')
+          join(__dirname, '@niddle-rs/niddle.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.win32-x64-msvc.node')
+            nativeBinding = require('./@niddle-rs/niddle.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('niddle-win32-x64-msvc')
+            nativeBinding = require('@niddle-rs/niddle-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'niddle.win32-ia32-msvc.node')
+          join(__dirname, '@niddle-rs/niddle.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.win32-ia32-msvc.node')
+            nativeBinding = require('./@niddle-rs/niddle.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('niddle-win32-ia32-msvc')
+            nativeBinding = require('@niddle-rs/niddle-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'niddle.win32-arm64-msvc.node')
+          join(__dirname, '@niddle-rs/niddle.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.win32-arm64-msvc.node')
+            nativeBinding = require('./@niddle-rs/niddle.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('niddle-win32-arm64-msvc')
+            nativeBinding = require('@niddle-rs/niddle-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'niddle.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, '@niddle-rs/niddle.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./niddle.darwin-universal.node')
+        nativeBinding = require('./@niddle-rs/niddle.darwin-universal.node')
       } else {
-        nativeBinding = require('niddle-darwin-universal')
+        nativeBinding = require('@niddle-rs/niddle-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'niddle.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, '@niddle-rs/niddle.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.darwin-x64.node')
+            nativeBinding = require('./@niddle-rs/niddle.darwin-x64.node')
           } else {
-            nativeBinding = require('niddle-darwin-x64')
+            nativeBinding = require('@niddle-rs/niddle-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'niddle.darwin-arm64.node')
+          join(__dirname, '@niddle-rs/niddle.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.darwin-arm64.node')
+            nativeBinding = require('./@niddle-rs/niddle.darwin-arm64.node')
           } else {
-            nativeBinding = require('niddle-darwin-arm64')
+            nativeBinding = require('@niddle-rs/niddle-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'niddle.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, '@niddle-rs/niddle.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./niddle.freebsd-x64.node')
+        nativeBinding = require('./@niddle-rs/niddle.freebsd-x64.node')
       } else {
-        nativeBinding = require('niddle-freebsd-x64')
+        nativeBinding = require('@niddle-rs/niddle-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-x64-musl.node')
+            join(__dirname, '@niddle-rs/niddle.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-x64-musl.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-x64-musl.node')
             } else {
-              nativeBinding = require('niddle-linux-x64-musl')
+              nativeBinding = require('@niddle-rs/niddle-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-x64-gnu.node')
+            join(__dirname, '@niddle-rs/niddle.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-x64-gnu.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('niddle-linux-x64-gnu')
+              nativeBinding = require('@niddle-rs/niddle-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-arm64-musl.node')
+            join(__dirname, '@niddle-rs/niddle.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-arm64-musl.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('niddle-linux-arm64-musl')
+              nativeBinding = require('@niddle-rs/niddle-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-arm64-gnu.node')
+            join(__dirname, '@niddle-rs/niddle.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-arm64-gnu.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('niddle-linux-arm64-gnu')
+              nativeBinding = require('@niddle-rs/niddle-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -226,26 +226,26 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-arm-musleabihf.node')
+            join(__dirname, '@niddle-rs/niddle.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-arm-musleabihf.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('niddle-linux-arm-musleabihf')
+              nativeBinding = require('@niddle-rs/niddle-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-arm-gnueabihf.node')
+            join(__dirname, '@niddle-rs/niddle.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-arm-gnueabihf.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('niddle-linux-arm-gnueabihf')
+              nativeBinding = require('@niddle-rs/niddle-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -255,26 +255,26 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-riscv64-musl.node')
+            join(__dirname, '@niddle-rs/niddle.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-riscv64-musl.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('niddle-linux-riscv64-musl')
+              nativeBinding = require('@niddle-rs/niddle-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'niddle.linux-riscv64-gnu.node')
+            join(__dirname, '@niddle-rs/niddle.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./niddle.linux-riscv64-gnu.node')
+              nativeBinding = require('./@niddle-rs/niddle.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('niddle-linux-riscv64-gnu')
+              nativeBinding = require('@niddle-rs/niddle-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -283,13 +283,13 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'niddle.linux-s390x-gnu.node')
+          join(__dirname, '@niddle-rs/niddle.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./niddle.linux-s390x-gnu.node')
+            nativeBinding = require('./@niddle-rs/niddle.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('niddle-linux-s390x-gnu')
+            nativeBinding = require('@niddle-rs/niddle-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
